4600370f0c456d9d288692112602179a89bfab4d
João Assêncio <joao.assencio123@gmail.com>
2023-09-11 18:03:18 -0300

    update

---
+++ js/script.js
@@ -0,0 +1,18 @@
+const accordionContent = document.querySelectorAll(".accordion-content");
+
+accordionContent.forEach((item, index) => {
+    let header = item.querySelector("header");
+    header.addEventListener("click", () => {
+        item.classList.toggle("open");
+
+        let description = item.querySelector(".accordion-description");
+        if(item.classList.contains("open")){
+            description.style.height = `${description.scrollHeight}px`;
+            item.querySelector("i").classList.replace("fa-plus","fa-minus");
+        }else{
+            description.style.height = "0px";
+            item.querySelector("i").classList.replace("fa-minus","fa-plus");
+        }
+        console.log(description)
+    })
+})
